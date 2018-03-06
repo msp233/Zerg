24ee714bb59f95dbb401748242daa42f00fb3321
v_shipmao(茆世平) <v_shipmao@tencent.com>
2018-03-06 11:54:26 +0800

    修改日志配置 写错误500例子

--- application/config.php
+++ application/config.php
@@ -156,7 +156,7 @@ return [
 
     'log'                    => [
         // 日志记录方式，内置 file socket 支持扩展
-        'type'  => 'File',
+        'type'  => 'text',#File #默认File 当改成text时 不写入日志
         // 日志保存目录
         'path'  => LOG_PATH,
         // 日志记录级别
